13c14db7d3ea611cfb5e45f84d9eedb53fa1dfb8
Yakimov Alexander <yakimov.alex2354@gmail.com>
2020-04-13 20:14:24 +0300

    implement face recognition

---
+++ Assets/Scripts/FaceRecognition.cs
@@ -0,0 +1,45 @@
+﻿namespace Main
+{
+	using System;
+	using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+	using UnityEngine.UI;
+	using OpenCvSharp;
+	using OpenCvSharp.Demo;
+
+	public class FaceRecognition : WebCamera
+    {
+        public TextAsset faces;
+        public TextAsset eyes;
+        public TextAsset shapes;
+
+        private FaceProcessorLive<WebCamTexture> processor;
+
+        void Awake()
+        {
+			base.Awake();
+			base.forceFrontalCamera = true;
+
+			Debug.Log(shapes.bytes);
+
+			processor = new FaceProcessorLive<WebCamTexture>();
+			processor.Initialize(faces.text, eyes.text, shapes.bytes);
+
+			processor.DataStabilizer.Enabled = true;
+			processor.DataStabilizer.Threshold = 2.0;
+			processor.DataStabilizer.SamplesCount = 2;
+
+			processor.Performance.Downscale = 256;
+			processor.Performance.SkipRate = 0;
+        }
+
+		protected override bool ProcessTexture(WebCamTexture input, ref Texture2D output)
+		{
+			processor.ProcessTexture(input, TextureParameters);
+			processor.MarkDetected();
+			output = Unity.MatToTexture(processor.Image, output);
+			return true;
+		}
+	}
+}
